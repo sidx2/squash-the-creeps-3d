e5f971de5cfc4f0bc36119917f82e0c94d66888f
Siddhesh Pardeshi <siddheshpardeshi2@gmail.com>
2024-01-01 19:12:19 +0530

    Get! Set! Go!

---
+++ player.gd
@@ -0,0 +1,68 @@
+extends CharacterBody3D
+
+@export var speed = 14
+@export var fall_excelleration = 75
+@export var jump_impulse = 20
+@export var bounce_impulse = 16
+
+var target_velocity = Vector3.ZERO
+
+signal hit
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	#if Input.is_action_pressed("jump"):
+		#direction.y -= 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(position + direction, Vector3.UP)
+		$AnimationPlayer.speed_scale = 4
+		
+	else:
+		$AnimationPlayer.speed_scale = 1
+		
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	#target_velocity.y = direction.y * speed
+	
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_excelleration * delta)
+		
+	velocity = target_velocity
+	move_and_slide()		
+	
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		target_velocity.y = jump_impulse
+
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		
+		if collision.get_collider() == null:
+			continue
+			
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				mob.squash()
+				target_velocity.y = bounce_impulse
+
+				break
+
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
+func die():
+	hit.emit()
+	queue_free()
+
+func _on_mob_detector_body_entered(body):
+	die()
